fb794d40bdc77d01c7ea69d0c98132e18278d85e
zjz <zjzgrh163.com>
2020-03-26 18:02:23 +0800

    3/26 commit add Link highlighting

--- src/main/java/com/swjtu/zjz/component/LoginHandlerInterceptor.java
+++ src/main/java/com/swjtu/zjz/component/LoginHandlerInterceptor.java
@@ -1,20 +1,20 @@
 package com.swjtu.zjz.component;
 
+import org.springframework.ui.Model;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import java.util.Collection;
+import java.util.Map;
 
 public class LoginHandlerInterceptor implements HandlerInterceptor {
     //目标方法执行之前
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         Object user = request.getSession().getAttribute("loginUser");
-        HttpSession session = request.getSession();
         if(user == null){
-
             /* 这里是另外一种页面跳转，但是失败了*/
             /*
             //未登录,返回登录页
@@ -26,11 +26,12 @@ public class LoginHandlerInterceptor implements HandlerInterceptor {
             return false;
             request.setAttribute("msg","没有权限，请先登录");
         */
-            request.getSession().setAttribute("msg","没有权限，请先登录");
-            System.out.println(session.getAttribute("msg"));
+//            request.getSession().setAttribute("message","没有权限，请先登录");
+//            System.out.println(session.getAttribute("message"));
 //            System.out.println("测试一下：页面跳转前进行输出");
-            response.sendRedirect("/login");
 //            System.out.println("测试一下：页面跳转后进行输出");
+
+            response.sendRedirect("/login");
             return false;
         }else {
             //已登录
