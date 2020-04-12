0a193b1c942fba0ea0ee71214a7d82fb518474ea
zjz <zjzgrh@163.com>
2020-04-12 11:41:17 +0800

    commit test

--- src/main/java/com/swjtu/zjz/controller/rentController.java
+++ src/main/java/com/swjtu/zjz/controller/rentController.java
@@ -24,11 +24,6 @@ public class rentController {
     //房主查看房屋租赁申请请求，
     @GetMapping("/houseapply")
     public String houseapplylist(HttpSession session, Model model){
-        //OwnerAccount houseOwner = ownerAccountMapper.select((String) session.getAttribute("loginUser"));
-        //System.out.println(" 看看查出了什么东西，有没有身份证号 ：" + houseOwner);
-        //List<House> houseList = houseMapper.getApplyhouseList(houseOwner.getOwner_identitynum());
-        //System.out.println("看看这个列表是什么东西：" + houseList);
-        //model.addAttribute("houselists", houseList);
         List<House> houselists = joinMapper.getOwnerApplyHouse((Integer) session.getAttribute("userId"));
         System.out.println(houselists);
         model.addAttribute("houselists", houselists);
